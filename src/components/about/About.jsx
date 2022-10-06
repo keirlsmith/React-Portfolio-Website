a5cb3c6d3897174df8273e01648cfecf018f8444
keirlsmith <97139230+keirlsmith@users.noreply.github.com>
2022-10-05 20:40:09 -0400

    About Me Start

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './about.css';
+import ME2 from '../../assets/img/profilePic.jpg';
 
 const About = () => {
   return (
-    <section id='#about'>About</section>
+    <section id='about'>
+        <h5>Get To Know</h5>
+        <h2>About Me</h2>
+
+        <div className="container about__container">
+            <div className="about__me">
+                <div className="about__me-image">
+                    <img src={ME2} alt="" />
+                </div>
+            </div>
+
+            <div className="about__content">
+
+            </div>
+        </div>
+    </section>
   )
 }
 
--- src/components/header/nav/Nav.jsx
+++ src/components/header/nav/Nav.jsx
@@ -8,14 +8,14 @@ import {AiOutlineMessage} from 'react-icons/ai';
 import {useState} from 'react';
 
 const Nav = () => {
-  const [activeName, setActiveNav] = useState('#');
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-        <a href="#" className='active'><AiOutlineHome /></a>
-        <a href="#about"><AiOutlineUser /></a>
-        <a href="#experience"><BiBook /></a>
-        <a href="#photography"><AiOutlineCamera /></a>
-        <a href="#contact"><AiOutlineMessage /></a>
+        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+        <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+        <a href="#photography" onClick={() => setActiveNav('#photography')} className={activeNav === '#photography' ? 'active' : ''}><AiOutlineCamera /></a>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage /></a>
     </nav>
   )
 }
