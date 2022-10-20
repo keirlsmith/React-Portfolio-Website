44da4c4f32008e83ec7b29c7c5e9eeb1b35af8e5
keirlsmith <97139230+keirlsmith@users.noreply.github.com>
2022-10-20 17:53:12 -0400

    Finish Portfolio Section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,52 @@ import IMG4 from '../../assets/img/duck1.jpg';
 import IMG5 from '../../assets/img/grove1.jpg';
 import IMG6 from '../../assets/img/butterfly1.jpg';
 
+const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: 'Crypto Currency Dashboard & Financial Visualization',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 2,
+      image: IMG1,
+      title: 'Crypto Currency Dashboard & Financial Visualization',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 3,
+      image: IMG1,
+      title: 'Crypto Currency Dashboard & Financial Visualization',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 4,
+      image: IMG1,
+      title: 'Crypto Currency Dashboard & Financial Visualization',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 5,
+      image: IMG1,
+      title: 'Crypto Currency Dashboard & Financial Visualization',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 6,
+      image: IMG1,
+      title: 'Crypto Currency Dashboard & Financial Visualization',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    }
+]
+
+
 const portfolio = () => {
   return (
     <section id='portfolio'>
@@ -16,55 +62,23 @@ const portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-      
-          <article className='portfolio__item'>
-              <div className="portfolio__item-image">
-                  <img src={IMG1} alt="" />
-              </div>
-              <h3>Projects I've Completed</h3>
-              <a href="https://github.com" className='btn'>Github</a>
-              <a href="https://dribbble.com/shots/19689266-Storage-Rent-Mobile-App-iOS-Android-UI" className='btn btn-primary' target="_blank">Live Demo</a>
-          </article>
-          <article className='portfolio__item'>
-              <div className="portfolio__item-image">
-                  <img src={IMG1} alt="" />
-              </div>
-              <h3>Projects I've Completed</h3>
-              <a href="https://github.com" className='btn'>Github</a>
-              <a href="https://dribbble.com/shots/19689266-Storage-Rent-Mobile-App-iOS-Android-UI" className='btn btn-primary' target="_blank">Live Demo</a>
-          </article>
-          <article className='portfolio__item'>
-              <div className="portfolio__item-image">
-                  <img src={IMG1} alt="" />
-              </div>
-              <h3>Projects I've Completed</h3>
-              <a href="https://github.com" className='btn'>Github</a>
-              <a href="https://dribbble.com/shots/19689266-Storage-Rent-Mobile-App-iOS-Android-UI" className='btn btn-primary' target="_blank">Live Demo</a>
-          </article>
-          <article className='portfolio__item'>
-              <div className="portfolio__item-image">
-                  <img src={IMG1} alt="" />
-              </div>
-              <h3>Projects I've Completed</h3>
-              <a href="https://github.com" className='btn'>Github</a>
-              <a href="https://dribbble.com/shots/19689266-Storage-Rent-Mobile-App-iOS-Android-UI" className='btn btn-primary' target="_blank">Live Demo</a>
-          </article>
-          <article className='portfolio__item'>
-              <div className="portfolio__item-image">
-                  <img src={IMG1} alt="" />
-              </div>
-              <h3>Projects I've Completed</h3>
-              <a href="https://github.com" className='btn'>Github</a>
-              <a href="https://dribbble.com/shots/19689266-Storage-Rent-Mobile-App-iOS-Android-UI" className='btn btn-primary' target="_blank">Live Demo</a>
-          </article>
-          <article className='portfolio__item'>
-              <div className="portfolio__item-image">
-                  <img src={IMG1} alt="" />
-              </div>
-              <h3>Projects I've Completed</h3>
-              <a href="https://github.com" className='btn'>Github</a>
-              <a href="https://dribbble.com/shots/19689266-Storage-Rent-Mobile-App-iOS-Android-UI" className='btn btn-primary' target="_blank">Live Demo</a>
-          </article>
+          {
+            data.map(({id, image, title, github, demo}) => {
+              return (
+                  <article key={id} className='portfolio__item'>
+                      <div className="portfolio__item-image">
+                          <img src={image} alt={title} />
+                      </div>
+                      <h3>{title}</h3>
+                      <div className="portfolio__item-cta">
+                          <a href={github} className='btn'>Github</a>
+                          <a href={demo} className='btn btn-primary' target="_blank">Live Demo</a>
+                      </div>
+                  </article>
+              )
+            })
+          }
+        
         
       </div>
     </section>
