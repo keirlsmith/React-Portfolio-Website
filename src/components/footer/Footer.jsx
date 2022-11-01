48bc9a206bdc012658cdf65c98c9564c505662fb
keirlsmith <97139230+keirlsmith@users.noreply.github.com>
2022-10-31 21:03:43 -0400

    finish footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './footer.css';
+import {FaFacebookSquare} from 'react-icons/fa';
+import {FaLinkedin} from 'react-icons/fa';
+import {FaInstagramSquare} from 'react-icons/fa';
+import {FaTwitterSquare} from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
@@ -8,10 +13,30 @@ const Footer = () => {
 
         <ul className='permalinks'>
             <li><a href="#">Home</a></li>
-            <li><a href="#">About Me</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#experience">My Experience</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
         </ul>
+
+        <div className="footer__socials">
+            <a href="https://facebook.com">
+                <FaFacebookSquare />
+            </a>
+            <a href="https://instagram.com">
+                <FaTwitterSquare />
+            </a>
+            <a href="https://linkedin.com"> 
+                <FaLinkedin />
+            </a>
+            <a href="https://instagram.com">
+                <FaInstagramSquare />
+            </a>
+        </div>
+
+        <div className="footer__copyright">
+            <small>&copy; Keir L Smith. All rights reserved.</small>
+        </div>
     </footer>
 
   )
